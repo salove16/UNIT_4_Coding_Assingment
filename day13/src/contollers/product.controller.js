d6391ab4059ca5ec9ccd46e2ffa0af00cc63425f
salove16 <shashank.shalabh909@gmail.com>
2022-03-26 23:48:21 +0530

    Update product.controller.js

--- day13/src/contollers/product.controller.js
+++ day13/src/contollers/product.controller.js
@@ -6,114 +6,124 @@ const Product = require("../models/product.model");
 
 const router = express.Router();
 
-
 router.post("", async (req, res) => {
-    try {
-      const product= await Product.create(req.body);
-  
-      const products = await Product.find().lean().exec();
-  
-      client.set("products", JSON.stringify(products));
-  
-      return res.status(201).send(product);
-    } catch (err) {
-      return res.status(500).send({ message: err.message });
-    }
-  });
+  try {
+    const product = await Product.create(req.body);
 
-  router.get("", async (req, res) => {
-    try {
-        const page=req.query.page
-       const pagesize=req.query.pagesize
-       const skip=Math.ceil((page-1)*pagesize)
-    //    let products = await Product.find().skip(skip).limit(pagesize).lean().exec();
-    //    return res.status(200).send({ products, redis: false });
-       client.get("products", async function (err, fetchedproducts) {
-        if (fetchedproducts) {
-          const products = JSON.parse(fetchedproducts);
-        //   products.skip(skip).limit(pagesize)
-          return res.status(200).send({ products, redis: true });
-        } else {
-          try {
-            const page=req.query.page
-            const pagesize=req.query.pagesize
-            const skip=Math.ceil((page-1)*pagesize)
-            const products = await Product.find().skip(skip).limit(pagesize).lean().exec();
-  
-            client.set("products", JSON.stringify(products));
-  
-            return res.status(200).send({ products, redis: false });
-          } catch (err) {
-            return res.status(500).send({ message: err.message });
+    const products = await Product.find().lean().exec();
+
+    client.set("products", JSON.stringify(products));
+
+    return res.status(201).send(product);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+});
+
+router.get("", async (req, res) => {
+  try {
+    const page = req.query.page;
+    const pagesize = req.query.pagesize;
+    const skip = Math.ceil((page - 1) * pagesize);
+
+    if (page != null && pagesize != null) {
+      client.get(
+        `products.${page}.${pagesize}`,
+        async function (err, fetchedproducts) {
+          if (fetchedproducts) {
+            const products = JSON.parse(fetchedproducts);
+
+            return res.status(200).send({ products, redis: true });
+          } else {
+            try {
+              const page = req.query.page;
+              const pagesize = req.query.pagesize;
+              const skip = Math.ceil((page - 1) * pagesize);
+              const products = await Product.find()
+                .skip(skip)
+                .limit(pagesize)
+                .lean()
+                .exec();
+
+              client.set(
+                `products.${page}.${pagesize}`,
+                JSON.stringify(products)
+              );
+
+              return res.status(200).send({ products, redis: false });
+            } catch (err) {
+              return res.status(500).send({ message: err.message });
+            }
           }
         }
-      });
-  
-    } catch (err) {
-      return res.status(500).send({ message: err.message });
+      );
     }
-  });
-  
-  router.get("/:id", async (req, res) => {
-    try {
-      client.get(`products.${req.params.id}`, async function (err, fetchedProduct) {
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    client.get(
+      `products.${req.params.id}`,
+      async function (err, fetchedProduct) {
         if (fetchedProduct) {
           const product = JSON.parse(fetchedProduct);
-  
+
           return res.status(200).send({ product, redis: true });
         } else {
           try {
             const product = await Product.findById(req.params.id).lean().exec();
-  
+
             client.set(`products.${req.params.id}`, JSON.stringify(product));
-  
+
             return res.status(200).send({ product, redis: false });
           } catch (err) {
             return res.status(500).send({ message: err.message });
           }
         }
-      });
-    } catch (err) {
-      return res.status(500).send({ message: err.message });
-    }
-  });
+      }
+    );
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+});
 
+router.patch("/:id", async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    })
+      .lean()
+      .exec();
 
-  router.patch("/:id", async (req, res) => {
-    try {
-      const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-      })
-        .lean()
-        .exec();
-  
-      const products = await Product.find().lean().exec();
-  
-      client.set(`products.${req.params.id}`, JSON.stringify(product));
-      client.set("products", JSON.stringify(products));
-  
-      return res.status(200).send(product);
-    } catch (err) {
-      return res.status(500).send({ message: err.message });
-    }
-  });
-  
+    const products = await Product.find().lean().exec();
 
+    client.set(`products.${req.params.id}`, JSON.stringify(product));
+    client.set("products", JSON.stringify(products));
 
+    return res.status(200).send(product);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+});
 
-  router.delete("/:id", async (req, res) => {
-    try {
-      const product = await Product.findByIdAndDelete(req.params.id).lean().exec();
-  
-      const products = await Product.find().lean().exec();
-  
-      client.del(`products.${req.params.id}`);
-      client.set("products", JSON.stringify(products));
-  
-      return res.status(200).send(product);
-    } catch (err) {
-      return res.status(500).send({ message: err.message });
-    }
-  });
+router.delete("/:id", async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id)
+      .lean()
+      .exec();
 
-module.exports=router
+    const products = await Product.find().lean().exec();
+
+    client.del(`products.${req.params.id}`);
+    client.set("products", JSON.stringify(products));
+
+    return res.status(200).send(product);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+});
+
+module.exports = router;
